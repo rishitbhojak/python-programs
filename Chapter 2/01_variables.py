e9b1d5f10809547d5a944f597e019151455d0ba0
Rishit Bhojak <rajabhojak@gmail.com>
2021-02-24 21:26:09 +0530

    printing data types

--- Chapter 2/01_variables.py
+++ Chapter 2/01_variables.py
@@ -2,7 +2,16 @@ a = "Rishit"
 b = 345
 c = 45.32
 d = False
+e = None
 print(a)
 print(b)
 print(c)
 print(d)
+print(e)
+
+#printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
