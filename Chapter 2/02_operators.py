39580d4e0da1f935105a25f5943a56183b0745e5
Rishit Bhojak <rajabhojak@gmail.com>
2021-02-25 18:48:31 +0530

    conditional and logical operators

--- Chapter 2/02_operators.py
+++ Chapter 2/02_operators.py
@@ -12,3 +12,10 @@ c=(14>7)
 print(c)
 d=(14!=7)
 print(d)
+
+#Logical operators
+bool1 = True
+bool2 = False
+print("The value of Bool1 and Bool2 is", (bool1 and bool2))
+print("The value of Bool1 or Bool2 is", (bool1 or bool2))
+print("The value of not Bool2 is", (not bool2))
