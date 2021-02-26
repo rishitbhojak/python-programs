dcbb376fdefe0595cfa9629888200639041518a4
Rishit Bhojak <rajabhojak@gmail.com>
2021-02-26 22:30:26 +0530

    string character skip values

--- Chapter 3/02_string_slicing.py
+++ Chapter 3/02_string_slicing.py
@@ -3,10 +3,16 @@
 # String concatenation
 # c= greeting + name
 # print(c)
-name = "Rishit"
 # print(name[0])
 # print(name[1])
-print(name[0:3])
-print(name[1:4])
-print(name[:3])
-print(name[1:])
+# print(name[0:3])
+# print(name[1:4])
+# print(name[:3])
+# print(name[1:])
+
+#String Scheme =>             0  1  2  3  4 
+#The above scheme is same as -5 -4 -3 -2 -1 
+# print(name[-4:-1])
+name = "RishitBhojak"
+d = name[0::2]
+print(d)
