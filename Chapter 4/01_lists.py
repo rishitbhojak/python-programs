2f464b77e74256630c2934344b9b3a31ec82648a
Rishit Bhojak <rajabhojak@gmail.com>
2021-02-27 17:30:47 +0530

    list having different data types

--- Chapter 4/01_lists.py
+++ Chapter 4/01_lists.py
@@ -3,3 +3,11 @@ print(a)
 print(a[3]) #to print value at index 3
 a[0] = 99 #updating value at a particular index
 print(a[0])
+
+# we can create a list with differet data types
+c = ["Rishit", 45 , False, 6.8]
+print(c)
+print(c[0])
+print(c[1])
+print(c[2])
+print(c[3])
