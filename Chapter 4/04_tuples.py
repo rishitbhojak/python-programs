ff3359e73128421d7845bd2ae8092dc00ec8f7bb
Rishit Bhojak <rajabhojak@gmail.com>
2021-02-28 16:54:23 +0530

    tuples with a single element

--- Chapter 4/04_tuples.py
+++ Chapter 4/04_tuples.py
@@ -3,4 +3,7 @@ t = (1,2,4,5)
 # Printing the elements of a tuple
 print(t[0])
 
-# Note : Tuples cannot be updated
+#Note : Tuples cannot be updated
+#A tuple with a single element is denoted by a single element followed by a comma 
+t1 = (1,)
+print(t1) 
