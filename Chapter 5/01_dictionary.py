23c762b3c1af39ce05646e7dd0975d119ce9e150
Rishit Bhojak <rajabhojak@gmail.com>
2021-03-02 16:13:05 +0530

    printing key-value pairs

--- Chapter 5/01_dictionary.py
+++ Chapter 5/01_dictionary.py
@@ -1,5 +1,8 @@
 myDict = {
     "Fast" : "In a quick manner",
-    "Rishit" : "A Student"
+    "Rishit" : "A Student",
+    "Marks" : [1,2,3]
 } 
 print(myDict['Fast'])
+print(myDict['Rishit'])
+print(myDict['Marks'])
