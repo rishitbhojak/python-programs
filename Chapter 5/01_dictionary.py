403068f52fde023269e1589cc0ef4469d27f1315
Rishit Bhojak <rajabhojak@gmail.com>
2021-03-02 16:18:01 +0530

    dictionary in a dictionary

--- Chapter 5/01_dictionary.py
+++ Chapter 5/01_dictionary.py
@@ -1,8 +1,10 @@
 myDict = {
     "Fast" : "In a quick manner",
     "Rishit" : "A Student",
-    "Marks" : [1,2,3]
+    "Marks" : [1,2,3],
+    "anotherDict" : {'king' : 'gamer'}
 } 
 print(myDict['Fast'])
 print(myDict['Rishit'])
 print(myDict['Marks'])
+print(myDict['anotherDict'])
